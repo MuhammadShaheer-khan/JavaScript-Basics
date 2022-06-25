e3848bc50f68a0de30cba17a39740927ebf95341
Muhammad Shaheer Khan <57253861+MuhammadShaheer-khan@users.noreply.github.com>
2022-06-25 21:57:20 +0500

    Add files via upload
    JS basic coding.

---
+++ blockchain.js
@@ -0,0 +1,19 @@
+var a = 12;
+var b = 4;
+
+var z = a+b;
+
+console.log(z);
+
+var firstName = "Muhammad Moavia"; // String datatype and Camel
+
+// object datatype:
+var car = {
+    colour: "black",
+    engineNumber: 12345,
+    model: "BMW"
+}
+
+console.log(car.engineNumber);
+console.log(car.model);
+
